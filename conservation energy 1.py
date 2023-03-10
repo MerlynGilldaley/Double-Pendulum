c56ca4b9ad06688d78e845b4ea9eff6c4fa6e873
MerlynGilldaley <126161660+MerlynGilldaley@users.noreply.github.com>
2023-03-10 13:05:33 +0000

    Plotting energy using the Runge Kutta Method
    After creating the runge kutta function in the pendulum class, this was then used to plot the kinetic energy, potential energy and total energy of the pendulum. This can then be compared to the Euler method of integration to show how the conservation of energy is different for different methods of integration and to show that the Runge Kutta method of numerical integration is more presice than the Forward Euler method of numerical integration

--- conservation energy 1.py
+++ conservation energy 1.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 from Pendulum1 import Pendulum
 from conservation_laws import Conservation
 
-Pendulum1 = Conservation(1, 1, np.pi/4, 0, 0, 0, -9.81*1*(1-np.cos(np.pi/4)))
+Pendulum1 = Conservation(1, 1, np.pi/16, 0, 0, 0, -9.81*1*(1-np.cos(np.pi/4)))
 
 
 
-Pendulum3 = Conservation(1, 1, np.pi/4, 0, 0, 0, -9.81*1*(1-np.cos(np.pi/4)))
+Pendulum3 = Conservation(1, 1, np.pi/16, 0, 0, 0, -9.81*1*(1-np.cos(np.pi/4)))
 
 
 while Pendulum1.theta_0 >= 2*np.pi:
@@ -20,31 +20,29 @@ while Pendulum3.theta_0 >= 2*np.pi:
 while Pendulum3.theta_0 <= 0:
     Pendulum3.theta_0 = Pendulum3.theta_0 + 2*np.pi
 
-time = np.array(0)
-KE_1 = np.array(0)
-PE_1 = np.array(1-np.cos(np.pi/4))
+time1 = np.array(Pendulum1.time)
+KE_1 = np.array(Pendulum1.kinetic_energy)
+PE_1 = np.array(Pendulum1.potential_energy)
 
 
-KE_2 = np.array(0)
-PE_2 = np.array(1-np.cos(np.pi/4))
 
-KE_3 = np.array(0)
-PE_3 = np.array(1-np.cos(np.pi/4))
+time3 = np.array(Pendulum3.time)
+KE_3 = np.array(Pendulum3.kinetic_energy)
+PE_3 = np.array(Pendulum3.potential_energy)
 
 #time = []
 #KE = []
 #PE = []
 
-for i in range(0, 200):
+for i in range(0, 20000):
 
-    Pendulum1.update_angle(0.1)
-    #print(Pendulum1.omega_0)
-    #print(Pendulum1.kinetic_energy)
+    Pendulum1.update_angle(0.001)
     Pendulum1.energy()
 
 
     KE_1 = np.append(KE_1, Pendulum1.kinetic_energy)
     PE_1 = np.append(PE_1, Pendulum1.potential_energy)
+    time1 = np.append(time1, Pendulum1.time)
     #time.append(Pendulum1.time)
     #KE.append(Pendulum1.kinetic_energy)
     #PE.append(Pendulum1.potential_energy)
@@ -53,30 +51,31 @@ for i in range(0, 200):
 
     
     
-    Pendulum3.runge_kutta(0.1)
+    Pendulum3.runge_kutta(0.001)
     Pendulum3.energy()
     
     KE_3 = np.append(KE_3, Pendulum3.kinetic_energy)
     PE_3 = np.append(PE_3, Pendulum3.potential_energy)
-    
-    
-    time = np.append(time, i*0.1)
-    
+    time3 = np.append(time3, Pendulum3.time)  
     
 
-plt.plot(time, KE_1, "r")
-plt.plot(time, PE_1, "b")
-plt.plot(time, KE_1+PE_1, "orange")
+plt.plot(time1, KE_1, "r")
+plt.plot(time1, PE_1, "b")
+plt.plot(time1, KE_1+PE_1, "orange")
 plt.show()
 
 
-#plt.plot(time, KE_2, "r")
-#plt.plot(time, PE_2, "b")
-#plt.plot(time, KE_2+PE_2, "orange")
-#plt.show()
-
-plt.plot(time, KE_3, "r")
-plt.plot(time, PE_3, "b")
-plt.plot(time, KE_3+PE_3, "orange")
+plt.plot(time3, KE_3, "r")
+plt.plot(time3, PE_3, "b")
+plt.plot(time3, KE_3+PE_3, "orange")
 plt.show()
+
+plt.plot(time1, KE_1+PE_1, "r", label = "Euler")
+plt.plot(time3, KE_3+PE_3, "b", label = "RK")
+plt.legend()
+plt.xlim(0,20)
+plt.ylim(-0.3, -0.1)
+plt.show()
+
+
 
