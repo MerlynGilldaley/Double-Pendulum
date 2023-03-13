dd3715bd17983cbcef21cbd61bb0fc9653c2af59
MerlynGilldaley <126161660+MerlynGilldaley@users.noreply.github.com>
2023-03-13 11:09:19 +0000

    new function for runge kutta method
    A second integration method has been added to the class to update the angle and angular velocity of the double pendulum using the runge kutta method.

--- Pendulum2.py
+++ Pendulum2.py
@@ -35,5 +35,93 @@ class Double_Pendulum():
         self.omega2 -= (self.mass1/self.mass2)*(self.omega2 - self.omega1)*np.sin(self.d_theta)*dt
 
         self.time = self.time + dt
+        
+        
+        
+
+    
+    def runge_kutta(self, theta1, omega1, theta2, omega2, dt):
+        
+        def omegadot_1(theta1, theta2):
+        #calculates the angular acceleration of the first pendulum
+            
+            num = -self.g * (2*self.mass1+self.mass2)*np.sin(theta1)-self.mass2*self.g*np.sin(theta1-2*theta2)-2*self.mass2*np.sin(theta1-theta2) * (self.length2*(omega2)**2+self.length1*(omega1)**2 * np.cos(theta1-theta2))
+            den = self.length1 * (2*self.mass1+self.mass2-self.mass2 * np.cos(2*theta1 - 2* theta2))
+            return num/den
+        
+        def omegadot_2(theta1, theta2):
+        #calculates the angular acceleration of the second pendulum
+            
+            temp = (2*np.sin(theta1 - theta2))
+            num = temp * (self.length1 * (omega1)**2 * (self.mass1+self.mass2)+self.g*(self.mass1+self.mass2)*np.cos(theta1)+self.length2*(omega2)**2 * self.mass2*np.cos(theta1-theta2))            
+            den = self.length2 * (2*self.mass1+self.mass2-self.mass2*np.cos(2*theta1 - 2*theta2))
+            return num/den
+        
+        def thetadot_1(omega1):
+            return omega1
+        
+        def thetadot_2(omega2):
+            return omega2
+        
+        k1_1 = thetadot_1(self.omega1)
+        k1_2 = thetadot_2(self.omega2)
+        
+        m1_1 = omegadot_1(self.theta1, self.theta2)
+        m1_2 = omegadot_2(self.theta1, self.theta2)   
+        
+        
+        k2_1 = thetadot_1(self.omega1 + m1_1 * (1/2) * dt)
+        k2_2 = thetadot_2(self.omega2 + m1_2 * (1/2) * dt)
+        
+        m2_1 = omegadot_1(self.theta1 + k1_1/2 * dt, self.theta2 + k1_1/2 * dt)
+        m2_2 = omegadot_2(self.theta1 + k1_2/2 * dt, self.theta2 + k1_2/2 * dt)
+        
+        
+        k3_1 = thetadot_1(self.omega1 + m2_1/2 * dt)
+        k3_2 = thetadot_2(self.omega2 + m2_2/2 * dt)
+        
+        m3_1 = omegadot_1(self.theta1 + k2_1/2 * dt, self.theta2 + k2_1/2 * dt)
+        m3_2 = omegadot_2(self.theta1 + k2_2/2 * dt, self.theta2 + k2_2/2 * dt)
+        
+        
+        k4_1 = thetadot_1(self.omega1 + m3_1 * dt)
+        k4_2 = thetadot_2(self.omega2 + m3_2 * dt)
+        
+        m4_1 = omegadot_1(self.theta1 +k3_1 * dt, self.theta2 + k3_1 * dt)
+        m4_2 = omegadot_2(self.theta1 + k3_2 * dt, self.theta2 + k3_2 * dt)
+        
+        
+        
+        self.theta1 = self.theta1 + 1/6 * (k1_1 + 2*k2_1 + 2*k3_1 + k4_1) * dt
+        self.theta2 = self.theta2 + 1/6 * (k1_2 + 2*k2_2 + 2*k3_2 + k4_2) * dt
+        
+        self.omega1 = self.omega1 + 1/6 * (m1_1 + 2*m2_1 + 2*m3_1 + m4_1) * dt
+        self.omega2 = self.omega2 + 1/6 * (m2_1 + 2*m2_2 + 2*m3_2 + m4_2) * dt
+        
+        self.time = self.time + dt
+        
+        
+        
+        
+        
+        
+    
+    
+            
+        
+
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
     
