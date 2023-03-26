b750f55e7b314dd4595d6bcb10553629ce47f13c
MerlynGilldaley <126161660+MerlynGilldaley@users.noreply.github.com>
2023-03-26 11:59:54 +0100

    Editing the graphs
    Adding titles and axis labels to the plots created by this code

--- conservation energy 1.py
+++ conservation energy 1.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 from Pendulum1 import Pendulum
 from conservation_laws import Conservation
 
-Pendulum1 = Conservation(1, 1, np.pi/16, 0, 0, 0, -9.81*1*(1-np.cos(np.pi/4)))
+Pendulum1 = Conservation(1, 1, np.pi/16, 0, 0, 0, -9.81*1*(1-np.cos(np.pi/16)))
 
 
 
-Pendulum3 = Conservation(1, 1, np.pi/16, 0, 0, 0, -9.81*1*(1-np.cos(np.pi/4)))
+Pendulum3 = Conservation(1, 1, np.pi/16, 0, 0, 0, -9.81*1*(1-np.cos(np.pi/16)))
 
 
 while Pendulum1.theta_0 >= 2*np.pi:
@@ -23,6 +23,7 @@ while Pendulum3.theta_0 <= 0:
 time1 = np.array(Pendulum1.time)
 KE_1 = np.array(Pendulum1.kinetic_energy)
 PE_1 = np.array(Pendulum1.potential_energy)
+
 
 
 
@@ -59,23 +60,44 @@ for i in range(0, 20000):
     time3 = np.append(time3, Pendulum3.time)  
     
 
-plt.plot(time1, KE_1, "r")
-plt.plot(time1, PE_1, "b")
-plt.plot(time1, KE_1+PE_1, "orange")
-plt.show()
+plt.subplot(121)
+plt.title("Euler Method")
+plt.plot(time1, KE_1, "r", label="Kinetic Energy")
+plt.plot(time1, PE_1, "b", label="Potential Energy")
+plt.plot(time1, KE_1+PE_1, "orange", label="Total Energy")
+plt.xlabel("Time")
+plt.ylabel("Energy")
+plt.legend()
 
 
-plt.plot(time3, KE_3, "r")
-plt.plot(time3, PE_3, "b")
-plt.plot(time3, KE_3+PE_3, "orange")
+
+plt.subplot(122)
+plt.title("Runge-Kutta Method")
+plt.plot(time3, KE_3, "r", label="Kinetic Energy")
+plt.plot(time3, PE_3, "b", label="Potential Energy")
+plt.plot(time3, KE_3+PE_3, "orange", label="Total Energy")
+plt.xlabel("Time")
+plt.ylabel("Energy")
+plt.legend()
 plt.show()
 
 plt.plot(time1, KE_1+PE_1, "r", label = "Euler")
 plt.plot(time3, KE_3+PE_3, "b", label = "RK")
 plt.legend()
-plt.xlim(0,20)
-plt.ylim(-0.3, -0.1)
+#plt.xlim(0,20)
+#plt.ylim(-0.225, -0.150)
+plt.xlabel("Time")
+plt.ylabel("Energy")
 plt.show()
+
+total_1 = KE_1 + PE_1
+total_2 = KE_3 + PE_3
+
+change_1 = (total_1[1]-total_1[-1])/total_1[1]
+change_2 = (total_2[1]-total_2[-1])/total_2[1]
+
+print(change_1, change_2)
+
 
 
 
