1db433491c772ccccd84b653df56641d600deaa1
MerlynGilldaley <126161660+MerlynGilldaley@users.noreply.github.com>
2023-03-29 13:32:31 +0100

    READ ME

---
+++ READ ME.py
@@ -0,0 +1,25 @@
+PENDULUM SIMULATION:
+    
+    DOUBLE PENDULUM FOLDER:
+        This contains a class for the double pendulum (Pendulum2) and a subclass for the energy of the double pendulum
+        From this there are three files that call these classes to run the code:
+            1) angleVStime_double:
+            Can be used to plot the angle vs time of the double pendulum and also the x/y coordinates of the pendulum
+            2) plot_conservation:
+            Plots the kinetic, potential and total energy of the double pendulum
+            3) change in energy:
+            Plots the percentage change in total energy for different time steps between iterations
+        
+            
+        
+            
+    SINGLE PENDULUM FOLDER:
+        This contains a class for the single pendulum (Pendulum1) and a subclass for the energy (conservation_laws)
+        From this there are 4 files that call these classes to run the code:
+            1) angleVStime 
+            plots the angle of the simple pendulum agaisnt time.
+            2) conservaton energy 1 
+            plots the kinetic, potentail and total energy of the pendulum
+            3) small angle approx
+            plots the angleVStime plot against the small angle approximation of the pendulum
+    
